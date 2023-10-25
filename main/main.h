4f98558e774c72923acf06fcbfa02679340f8ad2
Mike <125614854+E64b@users.noreply.github.com>
2023-10-25 23:34:51 +0300

    Example

---
+++ main/main.h
@@ -0,0 +1,24 @@
+#include <FastLED.h>
+//#include <SmartMatrix.h>
+
+#define DATA1_PIN 23
+#define DATA2_PIN 22
+#define DATA3_PIN 21
+#define DATA4_PIN 19
+#define DATA5_PIN 18
+#define DATA6_PIN 5
+#define DATA7_PIN 17
+#define DATA8_PIN 16
+#define DATA9_PIN 4
+#define DATA10_PIN 0
+#define DATA11_PIN 2
+#define DATA12_PIN 15
+#define DATA13_PIN 27
+#define DATA14_PIN 14
+
+#define BRIGHTNESS 175
+#define LED_TYPE WS2812
+#define COLOR_ORDER GRB
+#define WIGTH 16
+#define HEIGHT 16
+#define NUM_LEDS (WIGTH * HEIGHT)
--- main/main.ino
+++ main/main.ino
@@ -1,37 +1,80 @@
-#include <FastLED.h>
-//#include <SmartMatrix.h>
+#include "main.h"
 
-#define NUM_LEDS 1792
-#define DATA_PIN 1
-#define DATA2_PIN 3
-
-#define BRIGHTNESS 100
-#define LED_TYPE WS2812
-#define COLOR_ORDER GRB
-
-CRGB ledsL[NUM_LEDS];
-CRGB ledsR[NUM_LEDS];
+CRGB matrix_1[NUM_LEDS];
+CRGB matrix_2[NUM_LEDS];
+CRGB matrix_3[NUM_LEDS];
+CRGB matrix_4[NUM_LEDS];
+CRGB matrix_5[NUM_LEDS];
+CRGB matrix_6[NUM_LEDS];
+CRGB matrix_7[NUM_LEDS];
+CRGB matrix_8[NUM_LEDS];
+CRGB matrix_9[NUM_LEDS];
+CRGB matrix_10[NUM_LEDS];
+CRGB matrix_11[NUM_LEDS];
+CRGB matrix_12[NUM_LEDS];
+CRGB matrix_13[NUM_LEDS];
+CRGB matrix_14[NUM_LEDS];
 
 void setup(){
-	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(ledsL, NUM_LEDS).setCorrection(TypicalLEDStrip);
-	FastLED.addLeds<LED_TYPE, DATA2_PIN, COLOR_ORDER>(ledsR, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	/*Init Serial*/
+	Serial.begin(115200);
+	while(!Serial){
+		}
+	Serial.print("Serial OK");
+
+	/*Init 14 channels*/
+	FastLED.addLeds<LED_TYPE, DATA1_PIN, COLOR_ORDER>(matrix_1, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA2_PIN, COLOR_ORDER>(matrix_2, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA3_PIN, COLOR_ORDER>(matrix_3, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA4_PIN, COLOR_ORDER>(matrix_4, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA5_PIN, COLOR_ORDER>(matrix_5, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA6_PIN, COLOR_ORDER>(matrix_6, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA7_PIN, COLOR_ORDER>(matrix_7, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA8_PIN, COLOR_ORDER>(matrix_8, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA9_PIN, COLOR_ORDER>(matrix_9, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA10_PIN, COLOR_ORDER>(matrix_10, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA11_PIN, COLOR_ORDER>(matrix_11, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA12_PIN, COLOR_ORDER>(matrix_12, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA13_PIN, COLOR_ORDER>(matrix_13, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.addLeds<LED_TYPE, DATA14_PIN, COLOR_ORDER>(matrix_14, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
 	FastLED.setBrightness(BRIGHTNESS);
 	}
 
 void loop(){
-	for(uint32_t i=0; i<=NUM_LEDS; i++){
-		ledsL[i]=CRGB::Red;
-		FastLED.show();
-		delay(100);
-		ledsL[i]=CRGB::Black;
+	for(uint16_t i=0; i<=NUM_LEDS; i++){
+		matrix_1[i]=CRGB::Red;
+		matrix_2[i]=CRGB::Red;
+		matrix_3[i]=CRGB::Red;
+		matrix_4[i]=CRGB::Red;
+		matrix_5[i]=CRGB::Red;
+		matrix_6[i]=CRGB::Red;
+		matrix_7[i]=CRGB::Red;
+		matrix_8[i]=CRGB::Red;
+		matrix_9[i]=CRGB::Red;
+		matrix_10[i]=CRGB::Red;
+		matrix_11[i]=CRGB::Red;
+		matrix_12[i]=CRGB::Red;
+		matrix_13[i]=CRGB::Red;
+		matrix_14[i]=CRGB::Red;
 		FastLED.show();
 		delay(100);
 		}
-	for(uint32_t i=0; i<=NUM_LEDS; i++){
-		ledsR[i]=CRGB::Red;
-		FastLED.show();
-		delay(100);
-		ledsR[i]=CRGB::Black;
+	for(uint16_t i=0; i<=NUM_LEDS; i++){
+		matrix_1[i]=CRGB::Black;
+		matrix_2[i]=CRGB::Black;
+		matrix_3[i]=CRGB::Black;
+		matrix_4[i]=CRGB::Black;
+		matrix_5[i]=CRGB::Black;
+		matrix_6[i]=CRGB::Black;
+		matrix_7[i]=CRGB::Black;
+		matrix_8[i]=CRGB::Black;
+		matrix_9[i]=CRGB::Black;
+		matrix_10[i]=CRGB::Black;
+		matrix_11[i]=CRGB::Black;
+		matrix_12[i]=CRGB::Black;
+		matrix_13[i]=CRGB::Black;
+		matrix_14[i]=CRGB::Black;
 		FastLED.show();
 		delay(100);
 		}
