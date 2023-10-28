fc393b778ea0a23eecfa376c99443e26126c57aa
Mike <125614854+E64b@users.noreply.github.com>
2023-10-28 19:03:16 +0300

    Example

--- main/graphics.cpp
+++ main/graphics.cpp
@@ -1,26 +1,23 @@
 #include "main.h"
 
 void Graphics(){
-	for(uint16_t i=0; i<=NUM_LEDS; i++){
-		matrix_1[i]=CRGB::Red;
-		matrix_2[i]=CRGB::Green;
-		matrix_3[i]=CRGB::Blue;
-		matrix_4[i]=CRGB::Aqua;
-		matrix_5[i]=CRGB::Amethyst;
-		matrix_6[i]=CRGB::Lime;
-		matrix_7[i]=CRGB::LimeGreen;
-		matrix_8[i]=CRGB::Orange;
-		matrix_9[i]=CRGB::Pink;
-		matrix_10[i]=CRGB::Silver;
-		matrix_11[i]=CRGB::Yellow;
-		matrix_12[i]=CRGB::GreenYellow;
-		matrix_13[i]=CRGB::White;
-		matrix_14[i]=CRGB::Purple;
+		fill_solid(matrix_1, 256, CRGB::Red);
+		fill_solid(matrix_2, 256, CRGB::Green);
+		fill_solid(matrix_3, 256, CRGB::Blue);
+		fill_solid(matrix_4, 256, CRGB::Aqua);
+		fill_solid(matrix_5, 256, CRGB::Amethyst);
+		fill_solid(matrix_6, 256, CRGB::Lime);
+		fill_solid(matrix_7, 256, CRGB::LimeGreen);
+		fill_solid(matrix_8, 256, CRGB::Orange);
+		fill_solid(matrix_9, 256, CRGB::Pink);
+		fill_solid(matrix_10, 256, CRGB::Silver);
+		fill_solid(matrix_11, 256, CRGB::Yellow);
+		fill_solid(matrix_12, 256, CRGB::GreenYellow);
+		fill_solid(matrix_13, 256, CRGB::White);
+		fill_solid(matrix_14, 256, CRGB::Purple);
 		FastLED.show();
-		Serial.print("Current LED ");
-		Serial.println(i);
-		delay(50);
-		}
+		delay(2500);
+		
 
 	for(uint16_t i=0; i<=NUM_LEDS; i++){
 		matrix_1[i]=CRGB::Black;
--- main/serial.cpp
+++ main/serial.cpp
@@ -1,3 +1,7 @@
 #include "main.h"
 
-void Serial_In(){}
+void Serial_In(){
+	while(Serial.available()>0){
+		char VAL=(char)Serial.read();
+		}
+	}
